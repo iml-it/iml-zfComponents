88a31a16f88a5b5ed3fa3deac93fa348194ac2a7
Michael Rolli <rolli@iml.unibe.ch>
2007-12-16 20:56:32 +0000

    fixed coding standard violations

--- library/Iml/Debug/Exception.php
+++ library/Iml/Debug/Exception.php
@@ -7,12 +7,14 @@
  *
  * This work is licensed under the Creative Commons Attribution-Share Alike 2.5
  * Switzerland License. To view a copy of this license, visit
- * http://creativecommons.org/licenses/by-sa/2.5/ch/ or send a letter to Creative
- * Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ * http://creativecommons.org/licenses/by-sa/2.5/ch/ or send a letter to
+ * Creative Commons, 171 Second Street, Suite 300, San Francisco, California,
+ * 94105, USA.
  *
  * @category   Iml
  * @package    Iml_Debug
- * @copyright  Copyright (c) 2007 Institute for Medical Education, University of Bern (http://www.iml.unibe.ch)
+ * @copyright  Copyright (c) 2007 Institute for Medical Education, University of
+ *             Bern (http://www.iml.unibe.ch)
  * @author     Michael Rolli <michael.rolli@iml.unibe.ch>
  * @license    http://creativecommons.org/licenses/by-sa/2.5/ch/     CC-By-Sa
  * @version    $Id$
@@ -31,9 +33,11 @@ require_once 'Zend/Exception.php';
  *
  * @category   Iml
  * @package    Iml_Debug
- * @copyright  Copyright (c) 2007 Institute for Medical Education, University of Bern (http://www.iml.unibe.ch)
+ * @copyright  Copyright (c) 2007 Institute for Medical Education, University of
+ *             Bern (http://www.iml.unibe.ch)
  * @author     Michael Rolli <michael.rolli@iml.unibe.ch>
  * @license    http://creativecommons.org/licenses/by-sa/2.5/ch/     CC-By-Sa
  */
 class Iml_Debug_Exception extends Zend_Exception
-{}
+{
+}
