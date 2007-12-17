e2588ebdcb2b9bfd0ac69df000f66e2c0fac0d49
Michael Rolli <rolli@iml.unibe.ch>
2007-12-17 15:41:05 +0000

    fixed a coding standard violation

--- library/Iml/Debug.php
+++ library/Iml/Debug.php
@@ -63,7 +63,8 @@ class Iml_Debug extends Zend_Debug
     public static function dumpLogEvents($events, $label=null, $echo=true)
     {
         if (!is_array($events)) {
-            throw new Iml_Debug_Exception('Array expected for argument 1, ' . gettype($events) . ' given');
+            throw new Iml_Debug_Exception('Array expected for argument 1, '
+					  . gettype($events) . ' given');
         }
         // format label
         $output = ($label===null) ? '' : trim(strip_tags($label)) . ': ';
