6c4594e48c636450fb45068c47b8b7da2f4cf515
Michael Rolli <rolli@iml.unibe.ch>
2008-03-10 07:49:15 +0000

    added docblocks

--- library/Iml/Auth/Adapter/Shibboleth.php
+++ library/Iml/Auth/Adapter/Shibboleth.php
@@ -1,10 +1,47 @@
 <?php
 
+/**
+ * IML ZendFramework Components
+ *
+ * LICENSE
+ *
+ * This work is licensed under the Creative Commons Attribution-Share Alike 2.5
+ * Switzerland License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-sa/2.5/ch/ or send a letter to Creative
+ * Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *
+ * @category   Iml
+ * @package    Iml_Auth
+ * @subpackage Adapters
+ * @copyright  Copyright (c) 2007 Institute for Medical Education, University of Bern (http://www.iml.unibe.ch)
+ * @author     Michael Rolli <michael.rolli@iml.unibe.ch>
+ * @license    http://creativecommons.org/licenses/by-sa/2.5/ch/     CC-By-Sa
+ * @version    $Id$
+ */
+
+/**
+ * Zend_Auth_Adapter_Interface
+ */
 require_once 'Zend/Auth/Adapter/Interface.php';
 
+/**
+ * @category   Iml
+ * @package    Iml_Auth
+ * @subpackage Adapters
+ * @copyright  Copyright (c) 2007 Institute for Medical Education, University of Bern (http://www.iml.unibe.ch)
+ * @author     Michael Rolli <michael.rolli@iml.unibe.ch>
+ * @license    http://creativecommons.org/licenses/by-sa/2.5/ch/     CC-By-Sa
+ */
 class Iml_Auth_Adapter_Shibboleth implements Zend_Auth_Adapter_Interface
 {
-    protected $identityField = 'SHIB_SWISS_EDUPERSON_ID';
+    /**
+     * Denotes the key in $_SERVER which holds the
+     * unique id of an authenticated user.
+     *
+     * @var Zend_Mail
+     */
+    protected $usernameField = 'HTTP_SHIB_SWISSEP_UNIQUEID';
+
     /**
      * Sets username and password for authentication
      *
